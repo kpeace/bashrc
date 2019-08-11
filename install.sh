61a26de3b984d3f8137d6366e210334adb8b1c05
Kramer Peace <kpeace1@gmail.com>
2019-08-11 16:59:45 +0300

    Add sourcing of bashrc

--- install.sh
+++ install.sh
@@ -1,1 +1,5 @@
 git clone https://github.com/magicmonty/bash-git-prompt.git ~/.bash/.bash-git-prompt --depth=1
+
+echo "if [ -f \"$(pwd)/bashrc\" ] ; then" >> ~/.bashrc
+echo "    . $(pwd)/bashrc" >> ~/.bashrc
+echo "fi" >> ~/.bashrc
